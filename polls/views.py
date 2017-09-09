c098ef6479be3826cb6df89408766ec82f2d6448
Daniel <d.cunniffe.ie@gmail.com>
2017-09-09 21:27:33 +0100

    Part 3, complete

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 from .models import Question
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,10 +12,7 @@ def index(request):
     return render(request, 'polls/index.html',context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question':question_id})
 
 def results(request, question_id):
